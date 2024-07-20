cafcc135e9d9f407382bda8433eba8566d56213d
sistracia <156994197+sistracia@users.noreply.github.com>
2024-07-20 20:18:50 +0700

    Change `favicon` and add `loading.tsx` to SSR page

--- apps/web-v1/src/app/layout.tsx
+++ apps/web-v1/src/app/layout.tsx
@@ -8,6 +8,20 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Hooore",
   description: "Turning Tech Dreams into Happy Realities",
+  icons: [
+    {
+      rel: "icon",
+      type: "image/x-icon",
+      url: "/favicon-light.ico",
+      media: "(prefers-color-scheme: light)",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      url: "/favicon-dark.ico",
+      media: "(prefers-color-scheme: dark)",
+    },
+  ],
 };
 
 export default function RootLayout({
