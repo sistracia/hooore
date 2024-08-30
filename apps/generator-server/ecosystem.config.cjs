a9abc45ef91a34e84e2ffa51980247e39c2fc3ac
sistracia <156994197+sistracia@users.noreply.github.com>
2024-08-30 08:50:58 +0700

    Update `generator-server`'s `pm2` config

--- apps/generator-server/ecosystem.config.cjs
+++ apps/generator-server/ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "generator-server",
-      script: "node --env-file=./.env ./dist/index.js",
+      script: "node --env-file=./.env ./dist/index.mjs",
     },
   ],
 };
