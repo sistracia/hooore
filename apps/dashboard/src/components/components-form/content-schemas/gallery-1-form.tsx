550e9c7b5315d080592bede44e2701df5611ffbd
sistracia <156994197+sistracia@users.noreply.github.com>
2024-11-17 10:12:32 +0700

    Finish `gallery` category

--- apps/dashboard/src/components/components-form/content-schemas/gallery-1-form.tsx
+++ apps/dashboard/src/components/components-form/content-schemas/gallery-1-form.tsx
@@ -6,5 +6,43 @@ import type { FormFields } from "../types";
 
 export const GALLERY_1_FORM_SCHEMA: FormFields<Gallery1Slug, Gallery1Props> = {
   slug: "gallery-1",
-  fields: [],
+  fields: [
+    {
+      type: "textarea",
+      name: "headline",
+      label: "Headline",
+      placeholder: "Enter the headline here",
+    },
+    {
+      type: "textarea",
+      name: "description",
+      label: "Description",
+      placeholder: "Enter the description here",
+    },
+    {
+      type: "field-array",
+      name: "images",
+      addFieldText: "Add Image",
+      labelPrefix: "Gallery",
+      fields: [
+        {
+          type: "input-file",
+          name: "image",
+          label: "Image",
+        },
+        {
+          type: "input-text",
+          name: "link",
+          label: "Link",
+          placeholder: "Enter the link here",
+        },
+        {
+          type: "input-text",
+          name: "tag",
+          label: "Tag",
+          placeholder: "Enter the tag here",
+        },
+      ],
+    },
+  ],
 };
--- apps/dashboard/src/components/components-form/content-schemas/gallery-3-form.tsx
+++ apps/dashboard/src/components/components-form/content-schemas/gallery-3-form.tsx
@@ -6,5 +6,62 @@ import type { FormFields } from "../types";
 
 export const GALLERY_3_FORM_SCHEMA: FormFields<Gallery3Slug, Gallery3Props> = {
   slug: "gallery-3",
-  fields: [],
+  fields: [
+    {
+      type: "textarea",
+      name: "headline",
+      label: "Headline",
+      placeholder: "Enter the headline here",
+    },
+    {
+      type: "textarea",
+      name: "description",
+      label: "Description",
+      placeholder: "Enter the description here",
+    },
+    {
+      type: "field-group",
+      name: "",
+      label: "Call To Action",
+      fields: [
+        {
+          type: "input-text",
+          name: "cta_button_label",
+          label: "Button Label",
+          placeholder: "Enter the label here",
+        },
+        {
+          type: "input-text",
+          name: "cta_link",
+          label: "Link",
+          placeholder: "Enter the link here",
+        },
+      ],
+    },
+    {
+      type: "field-array",
+      name: "images",
+      addFieldText: "Add Image",
+      labelPrefix: "Gallery",
+      fields: [
+        {
+          type: "input-file",
+          name: "image",
+          label: "Image",
+        },
+        {
+          type: "input-text",
+          name: "link",
+          label: "Link",
+          placeholder: "Enter the link here",
+        },
+        {
+          type: "input-text",
+          name: "tag",
+          label: "Tag",
+          placeholder: "Enter the tag here",
+        },
+      ],
+    },
+  ],
 };
